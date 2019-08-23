333bb7c60909f5342a432f4d13d0be9c8d81fe4a
LeonardoSantanaFReire <leonardo_davi2014@hotmail.com>
2019-08-23 14:00:39 -0300

    curso java

---
+++ CursoJava/ExemploGradle/src/test/java/br/com/jobss/ExemploGradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.jobss.ExemploGradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExemploGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ CursoJava/ExemploMVN/src/main/java/br/com/jobss/ExemploMVN/ExemploMvnApplication.java
@@ -0,0 +1,13 @@
+package br.com.jobss.ExemploMVN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMvnApplication.class, args);
+	}
+
+}
